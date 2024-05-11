857c754a3e8d8bbd358ba5ab04dc806c6cfe0897
Christian <kristian2015ok@gmail.com>
2024-05-11 20:46:12 +0700

    Create: user repository

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Blog\Repository;
+
+use Blog\Domain\User;
+
+class UserRepository
+{
+    public function __construct(
+        private \PDO $connection
+    ) {
+    }
+
+    public function save(User $user): User
+    {
+        $statement = $this->connection->prepare("INSERT INTO users(id, email, username, password) VALUES(?, ?, ?, ?)");
+        $statement->execute([
+            $user->id,
+            $user->email,
+            $user->email,
+            $user->password
+        ]);
+        return $user;
+    }
+
+    public function findById(string $id): ?User
+    {
+        $statement = $this->connection->prepare("SELECT id, email, username, password FROM users WHERE id = ?");
+        $statement->execute([$id]);
+
+        try {
+            if ($row = $statement->fetch()) {
+                $user = new User();
+                $user->id = $row['id'];
+                $user->email = $row['email'];
+                $user->username = $row['username'];
+                $user->password = $row['password'];
+                return $user;
+            } else {
+                return null;
+            }
+        } finally {
+            $statement->closeCursor();
+        }
+    }
+
+    public function deleteAll(): void
+    {
+        $this->connection->exec("DELETE FROM users");
+    }
+}
