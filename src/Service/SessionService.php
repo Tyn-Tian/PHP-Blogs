0697f6a6b4b3466e97bea04b659dca5e3a99c0a2
Christian <kristian2015ok@gmail.com>
2024-05-13 15:22:18 +0700

    Create: session service

---
+++ src/Service/SessionService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Blog\Service;
+
+use Blog\Domain\Session;
+use Blog\Domain\User;
+use Blog\Repository\SessionRepository;
+use Blog\Repository\UserRepository;
+
+class SessionService
+{
+    public static string $COOKIE_NAME = "X-TYN-SESSION";
+
+    public function __construct(
+        private SessionRepository $sessionRepository,
+        private UserRepository $userRepository
+    ) {
+    }
+
+    public function create(string $userId): Session
+    {
+        $session = new Session();
+        $session->id = uniqid();
+        $session->userId = $userId;
+
+        $this->sessionRepository->save($session);
+
+        setcookie(self::$COOKIE_NAME, $session->id, time() + (60 * 60 * 24 * 30), '/');
+
+        return $session;
+    }
+
+    public function destory()
+    {
+        $sessionId = $_COOKIE[self::$COOKIE_NAME] ?? "";
+        $this->sessionRepository->deleteById($sessionId);
+
+        setcookie(self::$COOKIE_NAME, '', 1, "/");
+    }
+
+    public function current(): User
+    {
+        $sessionId = $_COOKIE[self::$COOKIE_NAME] ?? "";
+
+        $session = $this->sessionRepository->findById($sessionId);
+        if ($session == null) {
+            return null;
+        }
+
+        return $this->userRepository->findById($session->userId);
+    }
+}
