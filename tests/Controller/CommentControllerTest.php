4288dd5c55b8ae0fae9ab4468f1e1a2958768b51
Christian <kristian2015ok@gmail.com>
2024-05-21 14:14:16 +0700

    Test: comment controller delete comment

---
+++ tests/Controller/CommentControllerTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Blog\Controller;
+
+use Blog\Config\Database;
+use Blog\Domain\Blog;
+use Blog\Domain\Comment;
+use Blog\Domain\Session;
+use Blog\Domain\User;
+use Blog\Repository\BlogRepository;
+use Blog\Repository\CommentRepository;
+use Blog\Repository\SessionRepository;
+use Blog\Repository\UserRepository;
+use Blog\Service\SessionService;
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . "/../helper/Helper.php";
+
+class CommentControllerTest extends TestCase
+{
+    private CommentController $commentController;
+    private BlogRepository $blogRepository;
+    private CommentRepository $commentRepository;
+    private UserRepository $userRepository;
+
+    protected function setUp(): void
+    {
+        $this->commentRepository = new CommentRepository(Database::getConnection());
+        $this->blogRepository = new BlogRepository(Database::getConnection());
+        $sessionRepository = new SessionRepository(Database::getConnection());
+        $this->userRepository = new UserRepository(Database::getConnection());
+        $this->commentController = new CommentController();
+
+        $this->commentRepository->deleteAll();
+        $this->blogRepository->deleteAll();
+        $sessionRepository->deleteAll();
+        $this->userRepository->deleteAll();
+
+        $user = new User();
+        $user->id = "userId";
+        $user->email = "test@gmail.com";
+        $user->username = "testName";
+        $user->password = password_hash("testPass", PASSWORD_BCRYPT);
+        $this->userRepository->save($user);
+
+        $session = new Session();
+        $session->id = uniqid();
+        $session->userId = $user->id;
+        $sessionRepository->save($session);
+        $_COOKIE[SessionService::$COOKIE_NAME] = $session->id;
+
+        putenv("mode=test");
+    }
+
+    public function testPostDeleteCommentSuccess()
+    {
+        $blog = new Blog();
+        $blog->id = uniqid();
+        $blog->title = "testTitle";
+        $blog->content = "testContent";
+        $blog->userId = "userId";
+        $this->blogRepository->save($blog);
+
+        $comment = new Comment();
+        $comment->id = uniqid();
+        $comment->content = "testContent";
+        $comment->userId = "userId";
+        $comment->blogId = $blog->id;
+        $this->commentRepository->save($comment);
+
+        $this->commentController->postDeleteComment($comment->id);
+        $this->expectOutputRegex("[Location: /blog-$blog->id/detail]");
+    }
+
+    public function testPostDeleteCommentValidationError()
+    {
+        $otherUser = new User();
+        $otherUser->id = uniqid();
+        $otherUser->email = "test1@gmail.com";
+        $otherUser->username = "test1Name";
+        $otherUser->password = password_hash("testPass", PASSWORD_BCRYPT);
+        $this->userRepository->save($otherUser);
+
+        $blog = new Blog();
+        $blog->id = uniqid();
+        $blog->title = "testTitle";
+        $blog->content = "testContent";
+        $blog->userId = "userId";
+        $this->blogRepository->save($blog);
+
+        $comment = new Comment();
+        $comment->id = uniqid();
+        $comment->content = "testContent";
+        $comment->userId = $otherUser->id;
+        $comment->blogId = $blog->id;
+        $this->commentRepository->save($comment);
+
+        $this->commentController->postDeleteComment($comment->id);
+        $this->expectOutputRegex("[Location: /blog-$blog->id/detail]");
+    }
+}
